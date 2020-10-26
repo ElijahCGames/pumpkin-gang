57a4d7c03fffea57087d27293cf14f3a34575aaa
book81able <ecobb81@gmail.com>
2020-10-26 01:46:15 -0400

    Cleaning up
    Cleaned up the folder strucutre, adding music, making the movement feel better, and making a prefab for the pumpkin

--- Pumpkin Rolling/Assets/Scriprs/Roll.cs
+++ Pumpkin Rolling/Assets/Scriprs/Roll.cs
@@ -4,31 +4,51 @@ using UnityEngine;
 
 public class Roll : MonoBehaviour
 {
-    private Rigidbody rb;
-    private Animator anim;
+    
+    public Animator anim;
+    public Transform mesh;
+    public Transform movement;
+
     public float push;
     public float jump;
 
     public Transform playerInputSpace;
 
+    private Rigidbody rb;
+    private Collider collider;
+
+    private bool isGrounded;
+
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-        anim = gameObject.transform.GetComponentInParent<Animator>();
+        collider = gameObject.GetComponent<Collider>();
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
         rb.AddTorque(playerInputSpace.TransformDirection(new Vector3(y, 0, -x)) * push);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.AddForce(new Vector3(0, jump, 0));
             anim.SetTrigger("Jump");
+
+            isGrounded = false;
         }
-     
+    }
+
+    private void Update()
+    {
+        movement.position = transform.position;
+        mesh.rotation = transform.rotation;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
     }
 }
