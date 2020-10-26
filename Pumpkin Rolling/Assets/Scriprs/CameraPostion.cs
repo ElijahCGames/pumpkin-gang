8eee0159e95dc1c6826d4b9135ad756326d98846
book81able <ecobb81@gmail.com>
2020-10-26 01:06:05 -0400

    Initital push

---
+++ Pumpkin Rolling/Assets/Scriprs/CameraPostion.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPostion : MonoBehaviour
+{
+
+    public Transform focus;
+
+    [SerializeField, Range(3f, 20f)]
+    float distance = 5f;
+
+    [SerializeField, Min(0f)]
+    float focusRadius = 1f;
+
+    [SerializeField, Range(0f, 1f)]
+    float focusCentering = 0.5f;
+
+    [SerializeField, Range(1f, 360f)]
+    float rotationSpeed = 90f;
+
+
+    public float MouseSensitivity;
+
+    [SerializeField, Range(-89f, 89f)]
+    float minVerticalAngle = -30f, maxVerticalAngle = 60f;
+
+    private Vector3 focusPoint;
+
+    private Vector2 orbitAngles = new Vector2(45f, 0f);
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        focusPoint = focus.position;
+        transform.localRotation = Quaternion.Euler(orbitAngles);
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Screen.lockCursor = false;
+        }
+        else
+        {
+            Screen.lockCursor = true;
+        }
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        UpdateFocusPoint();
+        ManualRotation();
+        Quaternion lookRotation;
+        if (ManualRotation())
+        {
+            ConstrainAngles();
+            lookRotation = Quaternion.Euler(orbitAngles);
+        }
+        else
+        {
+            lookRotation = transform.localRotation;
+        }
+        Vector3 lookDirection = lookRotation * Vector3.forward;
+        Vector3 lookPosition = focusPoint - lookDirection * distance;
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
+    }
+
+    void OnValidate()
+    {
+        if (maxVerticalAngle < minVerticalAngle)
+        {
+            maxVerticalAngle = minVerticalAngle;
+        }
+    }
+
+    void ConstrainAngles()
+    {
+        orbitAngles.x =
+            Mathf.Clamp(orbitAngles.x, minVerticalAngle, maxVerticalAngle);
+
+        if (orbitAngles.y < 0f)
+        {
+            orbitAngles.y += 360f;
+        }
+        else if (orbitAngles.y >= 360f)
+        {
+            orbitAngles.y -= 360f;
+        }
+    }
+
+    void UpdateFocusPoint()
+    {
+        Vector3 targetPoint = focus.position;
+
+        if (focusRadius > 0f)
+        {
+            float distance = Vector3.Distance(targetPoint, focusPoint);
+
+            float t = 1f;
+
+            if (distance > 0.01f && focusCentering > 0f)
+            {
+                t = Mathf.Pow(1f - focusCentering, Time.deltaTime);
+            }
+            if (distance > focusRadius)
+            {
+                t = Mathf.Min(t, focusRadius / distance);
+            }
+
+            focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);
+        }
+        else
+        {
+            focusPoint = targetPoint;
+        }
+    }
+
+    bool ManualRotation()
+    {
+        Vector2 input = new Vector2(
+            Input.GetAxis("Vertical Camera"),
+            Input.GetAxis("Horizontal Camera")
+        );
+        const float e = 0.001f;
+        if (input.x < -e || input.x > e || input.y < -e || input.y > e)
+        {
+            orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input * MouseSensitivity;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Pumpkin Rolling/Assets/Scriprs/Roll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roll : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Animator anim;
+    public float push;
+    public float jump;
+
+    public Transform playerInputSpace;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        anim = gameObject.transform.GetComponentInParent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rb.AddTorque(playerInputSpace.TransformDirection(new Vector3(y, 0, -x)) * push);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector3(0, jump, 0));
+            anim.SetTrigger("Jump");
+        }
+     
+    }
+}
